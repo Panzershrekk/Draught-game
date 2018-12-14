2472785eb6adde52cc3eed86b69b1493987c56f1
Thomas Fossaert <thomas.fossaert@epitech.eu>
2018-12-14 16:51:22 +0100

    Commenting

--- draught.py
+++ draught.py
@@ -6,7 +6,7 @@ from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal, QSize
 from PyQt5.QtGui import QPainter, QColor, QFont
 import sys, random
 
-
+### Class to get useful information about the current ai moving such as its previous position or its actual one and the opportunity for each movement
 class AiData():
     def __init__(self):
         self.oldX = 0
@@ -15,11 +15,14 @@ class AiData():
         self.curY = 0
         self.opportunity = 0
 
+### Class tha will handle the simulation of a min max algorithm and retrieve the best possible move dor a piece
 class Ai():
     def __init__(self):
+        ### The depth of the min max algorithm, the deeper it goes the stronger that AI will be
         self.depth = 1
 
 
+    ### Get all data of each pawn on the current board
     def updatePawn(self, boardData):
         self.board = boardData
         self.AiPawn = []
@@ -37,9 +40,11 @@ class Ai():
                     self.PlayerPawn.append(boardData[y][x])
                     nbrOfPawn += 1
 
+    ### Calculate the best move at the end of the simulation (not fully done)
     def evaluate(self, data):
         return (data)
 
+    ### Simulate a movement of a pawn for Player X and calculate its opportuinity
     def simulate(self, x, y, player, king, data):
         data.oldX = x
         data.oldY = y
@@ -94,11 +99,12 @@ class Ai():
                     data.opportunity += 2
 
 
-
+    ### Remove the last simulated move (not used)
     def removeLastMove(self, pawn, data):
         self.board[data.curX][data.curY].belongToPlayer = 0
         self.board[data.oldX][data.oldY].belongToPlayer = 2
 
+    ### Retrieve the best possible move for the ai's opponent taking the ai previous move into account
     def minV(self, depth, prevData):
         finalData = AiData()
         finalData.opportunity = 1000000000
@@ -114,6 +120,7 @@ class Ai():
             #self.removeLastMove(pawn, data)
         return finalData
 
+    ### Retrieve the best possible move for the ai the oppenent previous move into account
     def maxV(self, depth, prevData):
         finalData = AiData()
         finalData.opportunity = -1000000000
@@ -128,11 +135,13 @@ class Ai():
                 finalData = val
         return finalData
 
+    ### Launch the simulation and return the best move for the ai
     def defineBest(self):
 
         i = self.maxV(self.depth, None)
         return i
 
+### Class that contain data about pawn, what position they have, to who they belong, if they are king etc
 class Pawn():
     def __init__(self, x, y, player):
         super().__init__()
@@ -143,33 +152,38 @@ class Pawn():
         self.moveAvailable = False
         self.isKing = False
 
+    ### Retrieve who own the pawn
     def getPlayer(self):
         return self.belongToPlayer
 
 
+### Information about the player
 class Player():
     def __init__(self, player):
         super().__init__()
+        ### Player 1 or 2
         self.player = player
+        ### Number of pawn left
         self.pawnLeft = 12
+        ### Number of pawn killed
         self.pawnKilled = 0
 
-
-
+### Class that contains data about the board of the game
 class BoardData():
     def __init__(self):
         super().__init__()
         self.initData()
 
+    ### Create arrays that will contain the data of each square of the board
     def initData(self):
         self.boardData = [[0 for x in range(8)] for y in range(8)]
-        self.playerPawn = []
 
+    ### Create pawn for player x at position x and y
     def createPawn(self, x, y, player):
         self.boardData[x][y] = player
-        self.playerPawn.append(Pawn(0, 0, 1))
         return (Pawn(x, y, player))
 
+    ### Make the move for the pawn of player X
     def movePawn(self, x, y, player, newX, newY, newP):
         self.boardData[x][y].x = newX
         self.boardData[x][y].y = newY
@@ -180,12 +194,14 @@ class BoardData():
         self.boardData[newX][newY].y = newY
         self.boardData[newX][newY].belongToPlayer = newP
 
+    ### Delete all GUI information after a move
     def clearBoard(self):
         for i in range(8):
             for j in range(8):
                 self.boardData[j][i].moveAvailable = False
                 self.boardData[j][i].selected = False
 
+    ### Generate every pawn for each player
     def setupBoardData(self, x, y):
         for i in range(y):
             for j in range(x):
@@ -219,7 +235,7 @@ class BoardData():
         self.boardData[6][5] = self.createPawn(6, 5, 2)
         # print(self.boardData[0][0], self.boardData[0][1], self.boardData[0][2])
 
-
+### Class that will display who is the winner of the game
 class WinnerWindow(QDialog):
     def __init__(self, winner):
         super().__init__()
@@ -239,13 +255,16 @@ class WinnerWindow(QDialog):
         self.setLayout(grid)
         self.setWindowTitle("And we have a winner !")
 
+    ### Set the draught class
     def setDraught(self, draught):
         self.draught = draught
 
+    ### Restart the game
     def restart(self):
         self.draught.resetGame()
         self.close()
 
+### Main class that will launch the game
 class Draught(QMainWindow):
 
     def __init__(self):
@@ -254,10 +273,12 @@ class Draught(QMainWindow):
         self.initUI()
 
     def initUI(self):
+        ### Instantitate the board class
         self.tboard = Board(self)
         self.tboard.setDraught(self)
         self.setCentralWidget(self.tboard)
 
+        ### Create the menu and action
         mainMenu = self.menuBar()
         gameMenu = mainMenu.addMenu(" Game")
 
@@ -279,9 +300,7 @@ class Draught(QMainWindow):
         pve.triggered.connect(self.aiGame)
 
 
-        self.statusbar = self.statusBar()
-        self.tboard.msg2Statusbar[str].connect(self.statusbar.showMessage)
-
+        ### Toolbar that will contain information about the game,
         toolbar = QToolBar()
         toolbar.setAllowedAreas(Qt.RightToolBarArea)
         toolbar.setIconSize(QSize(40, 40))
@@ -295,11 +314,12 @@ class Draught(QMainWindow):
         myFont.setBold(True)
         myFont.setPixelSize(15)
 
-        self.playerLabel = QLabel("Player")
+        ### Label of both player
+        self.playerLabel = QLabel("Player 1")
         self.playerLabel.setFont(myFont)
         self.playerTurn = self.turnLabel()
 
-        self.opponentLabel = QLabel("Opponent")
+        self.opponentLabel = QLabel("Player 2")
         self.opponentLabel.setFont(myFont)
         self.opponentLabel.setContentsMargins(0, 30, 0, 0)
         self.opponentTurn = self.turnLabel()
@@ -311,6 +331,7 @@ class Draught(QMainWindow):
         self.playerPawnKilled = QLabel(str(self.tboard.players[0].pawnKilled))
         self.opponentPawnKilled = QLabel(str(self.tboard.players[1].pawnKilled))
 
+        ### Putting all widget in grid
         self.grid.addWidget(self.playerLabel, 0, 0)
         self.grid.addWidget(self.playerTurn, 0, 1)
         self.grid.addWidget(self.remainingLabel(), 1, 0)
@@ -326,9 +347,10 @@ class Draught(QMainWindow):
         self.grid.addWidget(self.opponentPawnKilled, 5, 1)
 
         magicWidget.setLayout(self.grid)
-
+        ###Putting the grid in a toolbar
         toolbar.addWidget(magicWidget)
 
+        ### Update the label every turn
         self.updateTurnLabel()
 
         self.addToolBar(Qt.RightToolBarArea, toolbar)
@@ -338,6 +360,7 @@ class Draught(QMainWindow):
         self.setWindowTitle('Draught')
         self.show()
 
+    ### Reset the game
     def resetGame(self):
         self.tboard.initBoard()
         self.updatePawnLeft(0)
@@ -347,14 +370,17 @@ class Draught(QMainWindow):
         self.updateTurnLabel()
         self.update()
 
+    ### Set up the player vs player mode
     def playerGame(self):
         self.resetGame()
         self.tboard.aiActivated = False
 
+    ### Set up the player vs ai mode
     def aiGame(self):
         self.resetGame()
         self.tboard.aiActivated = True
 
+    ### Display who won the game
     def displayWinner(self, nextTurnPlayer):
         self.update()
         if (nextTurnPlayer == 0):
@@ -365,17 +391,21 @@ class Draught(QMainWindow):
         self.winnerWindow.setDraught(self)
         self.winnerWindow.exec()
 
+    ### Displaye who is the current turn
     def turnLabel(self):
         label = QLabel("Your turn !")
         label.setStyleSheet('color: #c20d00')
         return label
 
+    ### Display the number of remaining pawn
     def remainingLabel(self):
         return QLabel("Remaining : ")
 
+    ### Display the number of killed pawn
     def killedLabel(self):
         return QLabel("Killed : ")
 
+    ### Update the number of pawn left
     def updatePawnLeft(self, player):
         if (player == 0):
             self.playerPawnLeft.setText(str(self.tboard.players[0].pawnLeft))
@@ -383,6 +413,7 @@ class Draught(QMainWindow):
             self.opponentPawnLeft.setText(str(self.tboard.players[1].pawnLeft))
         self.update()
 
+    ### Update the number of pawn killed
     def updatePawnKilled(self, player):
         if (player == 0):
             self.playerPawnKilled.setText(str(self.tboard.players[0].pawnKilled))
@@ -390,6 +421,7 @@ class Draught(QMainWindow):
             self.opponentPawnKilled.setText(str(self.tboard.players[1].pawnKilled))
         self.update()
 
+    ### Update the turn of player
     def updateTurnLabel(self):
         if (self.tboard.currentPlayer == 1):
             self.playerTurn.setText("Your turn !")
@@ -405,10 +437,9 @@ class Draught(QMainWindow):
         self.move((screen.width() - size.width()) / 2,
                   (screen.height() - size.height()) / 2)
 
-
+### Class that will manage the display of the board and the behaviour of the game
 class Board(QFrame):
-    msg2Statusbar = pyqtSignal(str)
-
+    ### Size of the board
     BoardWidth = 8
     BoardHeight = 8
 
@@ -417,20 +448,30 @@ class Board(QFrame):
         self.aiActivated = False
         self.initBoard()
 
+    ### Initiliaze all data for the board
     def initBoard(self):
         self.timer = QBasicTimer()
+        ### Set the first player
         self.currentPlayer = 1
+        ### Setup ai
         self.Ai = Ai()
+        ### Check if the game is over
         self.gameFinished = False
+        ### Check wich pawn is selected
         self.pawnSelected = False
+        ### Check if the player can do other move
         self.forced = False
+        ### Get the selected pawn pos
         self.selectedX = 0
         self.selectedY = 0
+        ### Get the selected move
         self.curX = 0
         self.curY = 0
+        ### Setup the board
         self.board = []
         self.boardData = BoardData()
         self.boardData.setupBoardData(Board.BoardWidth, Board.BoardHeight)
+        ### Create players
         self.players = []
         self.players.append(Player(1))
         self.players.append(Player(2))
@@ -439,23 +480,22 @@ class Board(QFrame):
     def setDraught(self, parent):
         self.draught = parent
 
-    def shapeAt(self, x, y):
-        return self.board[(y * Board.BoardWidth) + x]
-
-    def setShapeAt(self, x, y, shape):
-        self.board[(y * Board.BoardWidth) + x] = shape
-
+    ### Get the width of the square
     def squareWidth(self):
         return self.contentsRect().width() // Board.BoardWidth
 
+    ### Get the height of the square
     def squareHeight(self):
         return self.contentsRect().height() // Board.BoardHeight
 
+    ### Paint event that will draw the board
     def paintEvent(self, event):
+        ### The painter
         painter = QPainter(self)
+        ### The rectangle
         rect = self.contentsRect()
-
         boardTop = rect.bottom() - Board.BoardHeight * self.squareHeight()
+        ### Draw every square of the board
         for i in range(Board.BoardHeight):
             for j in range(Board.BoardWidth):
                 self.drawSquare(painter, rect.left() + j * self.squareWidth(), boardTop + i * self.squareHeight(),
@@ -474,9 +514,9 @@ class Board(QFrame):
                     self.drawPawn(painter, rect.left() + j * self.squareWidth(), boardTop + i * self.squareHeight(), 1,
                                   self.boardData.boardData[j][i].isKing)
 
+    ### Method that will draw Square
     def drawSquare(self, painter, x, y, colorIndex):
         colorTable = [0x514D5B, 0xFFFFFF, 0x7F5579, 0xFF0000]
-        #colorTable = [0x514D5B, 0xFFFFFF, 0x7F5579]
 
         color = QColor(colorTable[colorIndex])
         painter.fillRect(x + 1, y + 1, self.squareWidth() - 2,
@@ -492,6 +532,7 @@ class Board(QFrame):
         painter.drawLine(x + self.squareWidth() - 1,
                          y + self.squareHeight() - 1, x + self.squareWidth() - 1, y + 1)
 
+    ### Function that will draw pawn
     def drawPawn(self, painter, x, y, colorIndex, isKing=False):
         colorTable = [0xC1442E, 0xF6CD37]
 
@@ -504,6 +545,7 @@ class Board(QFrame):
             painter.drawEllipse(x, y, self.squareWidth() / 2, self.squareHeight() / 2)
 
 
+    ### Check if a move can kill a pawn
     def killAvailable(self, x , y):
         if self.boardData.boardData[x][y].isKing == True or self.currentPlayer == 1:
             if x + 1 <= 7 and y + 1 <= 7:
@@ -526,6 +568,7 @@ class Board(QFrame):
                         return True
         return False
 
+    ### Define if the next ove can kill
     def defineKillingMove(self, x , y):
         if self.boardData.boardData[x][y].isKing == True or self.currentPlayer == 1:
             if x + 1 <= 7 and y + 1 <= 7:
@@ -547,6 +590,7 @@ class Board(QFrame):
                     if x - 2 >= 0 and y - 2 >= 0 and self.boardData.boardData[x - 2][y - 2].belongToPlayer == 0:
                         self.boardData.boardData[x - 2][y - 2].moveAvailable = True
 
+    ### Check if the the pawn can move in direction
     def defineAvailableMove(self, x, y):
 
         if self.boardData.boardData[x][y].isKing == True or self.currentPlayer == 1:
@@ -585,6 +629,7 @@ class Board(QFrame):
                 else:
                     self.boardData.boardData[x - 1][y - 1].moveAvailable = True
 
+    ### Kill the pawn after a killing move move, return true a a pawn was killed
     def removePawn(self, oldX, oldY, x, y):
 
         killCheck = False
@@ -601,7 +646,6 @@ class Board(QFrame):
                 self.draught.updatePawnLeft(playerHurted)
                 self.draught.updatePawnKilled(self.currentPlayer - 1)
                 killCheck = True
-                ## TODO : mettre pawnLeft à 0
                 if (self.players[playerHurted].pawnLeft == 0):
                     self.gameFinished = True
             if cptX < x:
@@ -614,23 +658,28 @@ class Board(QFrame):
                 cptY -= 1
         return killCheck
 
+    ### The mouse pressed event
     def mousePressEvent(self, event):
+        ### Check if the game is finished
         if self.gameFinished == True:
             return
+        ### Get the position where the click was and check if pawn was previously selected, allowing the pawn to move
         if event.button() == Qt.LeftButton & self.pawnSelected == True:
             x = int(event.pos().x() / self.squareWidth())
             y = int(event.pos().y() / self.squareHeight())
 
+            ### End the turn if player is not forced to play
             if x == self.selectedX and y == self.selectedY and self.forced == False:
                 self.boardData.clearBoard()
                 self.pawnSelected = False
+            ###check if a move is available
             if self.boardData.boardData[x][y].moveAvailable == True:
                 self.boardData.movePawn(self.selectedX, self.selectedY, 1, x, y, self.currentPlayer)
                 self.boardData.boardData[self.selectedX][self.selectedY].selected = False
                 killCheck = self.removePawn(self.selectedX, self.selectedY, x, y)
 
                 self.boardData.clearBoard()
-
+                ### Check if multikill is available
                 if (self.killAvailable(x, y) == True and killCheck == True):
                     self.forced = True
                     self.selectedX = x
@@ -638,11 +687,13 @@ class Board(QFrame):
                     self.defineKillingMove(self.selectedX, self.selectedY)
                     self.boardData.boardData[x][y].selected = True
                 else:
+                    ### Setup the next player turn
                     self.pawnSelected = False
                     if (self.currentPlayer == 1):
                         if y == 7:
                             self.boardData.boardData[x][y].isKing = True
                         self.currentPlayer = 2
+                        ### Play the ai turn if ai is activated
                         if self.aiActivated is True and self.gameFinished == False:
                             self.Ai.updatePawn(self.boardData.boardData)
                             ai = self.Ai.defineBest()
@@ -658,10 +709,11 @@ class Board(QFrame):
                         self.currentPlayer = 1
                     self.forced = False
                     self.draught.updateTurnLabel()
-
+                ### Check if the game is finished
                 if self.gameFinished == True:
                     self.draught.displayWinner(self.currentPlayer - 1)
                     return
+        ### Click but no pawn selected
         elif event.button() == Qt.LeftButton:
             x = int(event.pos().x() / self.squareWidth())
             y = int(event.pos().y() / self.squareHeight())
@@ -676,11 +728,8 @@ class Board(QFrame):
 
         self.update()
 
-    def mouseMoveEvent(self, event):
-        x = int(event.pos().x() / self.squareWidth())
-        y = int(event.pos().y() / self.squareHeight())
 
-
+### Launch the game
 if __name__ == '__main__':
     app = QApplication([])
     draught = Draught()
